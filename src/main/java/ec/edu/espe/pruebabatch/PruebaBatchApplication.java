d8bbe204ef5e4c456d4292e1761c4e9dddc8d81d
María José Pazmiño <65615849+mjpazmino@users.noreply.github.com>
2022-08-17 19:55:42 -0500

    First commit

---
+++ src/main/java/ec/edu/espe/pruebabatch/PruebaBatchApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.pruebabatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ec/edu/espe/pruebabatch/PruebaBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.pruebabatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
